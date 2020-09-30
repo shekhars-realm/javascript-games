f77e248a17fe234b680c21018a239e1fc62d8b47
shekhars-realm <sbhattacharya099@gmail.com>
2020-09-30 23:15:41 +0200

    final commit

--- dinosour-404/script.js
+++ dinosour-404/script.js
@@ -2,8 +2,20 @@ var character = document.getElementById('character')
 var block = document.getElementById('block')
 
 function jump() {
-    character.classList.add('animate')
+    if(character.classList != 'animate') {
+        character.classList.add('animate')
+    }
     setTimeout(() => {
         character.classList.remove('animate')
     }, 500);
 }
+
+var checkDead = setInterval(() => {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        alert("You Lose!!!!!!!!!")
+    }
+}, 10);
